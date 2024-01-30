e42d3d0bee24002bac0da691cce2efeb88945b61
Barakiat <rekhadmob@gmail.com>
2024-01-30 13:46:25 +0400

    add jest

--- frontend/src/copmonents/ScrollTop/ScrollTop.test.tsx
+++ frontend/src/copmonents/ScrollTop/ScrollTop.test.tsx
@@ -5,7 +5,6 @@ import ScrollTop from "./ScrollTop";
 test("renders ScrollTop component", () => {
     render(<ScrollTop />);
 
-    // Проверяем, что компонент отрисован
     const scrollTopElement = screen.getByLabelText("scroll back to top");
     expect(scrollTopElement).toBeInTheDocument();
 });
